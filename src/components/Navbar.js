84bc34c36ff574fa62b4ea07fcc6f312b01374ea
Bart Kalanski <bkalanski@gmail.com>
2021-02-27 22:20:13 +0000

    Update link style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, Modal, Button, Link as LinkStyle } from "@material-ui/core/";
+import { AppBar, Toolbar, Typography, Modal, Button } from "@material-ui/core/";
 import BuildIcon from "@material-ui/icons/Build";
 
 const useStyles = makeStyles((theme) => ({
@@ -16,11 +16,18 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     padding: "0.5rem 4rem",
   },
+  link: {
+    textDecoration: "none",
+    color: "white",
+  },
   buildIcon: {
     cursor: "pointer",
   },
   modal: {
     position: "absolute",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
     height: "15rem",
     padding: "2rem",
     width: 400,
@@ -31,8 +38,15 @@ const useStyles = makeStyles((theme) => ({
     border: "none",
     boxShadow: theme.shadows[5],
   },
+  buttonContainer: {
+    alignSelf: 'flex-end',
+  },
+  startButton: {
+    background: '#647881',
+    padding: '0.5rem 2.25rem',
+  },
   text: {
-    color: "fff",
+    color: "white",
   },
 }));
 
@@ -52,12 +66,11 @@ const Navbar = () => {
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar variant="dense" className={classes.toolbar}>
-          <Link to="/">
-            <LinkStyle variant="h6" color="inherit">
+          <Link to="/" className={classes.link}>
+            <Typography variant="h6" color="inherit">
               Meshery
-            </LinkStyle>
+            </Typography>
           </Link>
-
           <BuildIcon onClick={handleOpen} className={classes.buildIcon} />
           <Modal
             open={open}
@@ -68,26 +81,32 @@ const Navbar = () => {
             <div className={classes.modal}>
               <Typography
                 variant="h4"
-                classes={{ colorTextPrimary: "#fff" }}
                 gutterBottom="true"
                 paragraph="true"
+                className={classes.text}
                 id="simple-modal-title"
               >
                 Configuration Wizard
               </Typography>
               <Typography
+                variant="body2"
                 gutterBottom="true"
                 paragraph="true"
+                className={classes.text}
                 id="simple-modal-description"
               >
                 Welcome to configuration wizard, Meshery's unique feature. The
                 configuration wizard represents an assembly of existing
                 functionality and settings that you can configure.
               </Typography>
-              <Button onClick={handleClose}>Skip</Button>
-              <Button onClick={handleClose}>
-                <Link to="/wizard">Get Started</Link>
+              <div className={classes.buttonContainer}>
+              <Button onClick={handleClose} className={classes.text}>Skip</Button>
+              <Button onClick={handleClose} className={classes.startButton}>
+                <Link to="/wizard" className={classes.link}>
+                  Get Started
+                </Link>
               </Button>
+              </div>
             </div>
           </Modal>
         </Toolbar>
