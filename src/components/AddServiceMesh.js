c1a1787131dc0737e513af409af94075a119b619
Bart Kalanski <bkalanski@gmail.com>
2021-03-02 21:07:38 +0000

    Add Cards

--- src/components/AddServiceMesh.js
+++ src/components/AddServiceMesh.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import {
+  makeStyles,
+  Container,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core/";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    marginBottom: "7rem",
+  },
+  card: {
+    height: "12rem",
+    width: "12rem",
+    margin: "1rem 2rem -1rem 2rem",
+    borderRadius: "15%",
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+  },
+  contentTop: {
+      background: 'black',
+      width: "100%",
+  },
+  contentBottom: {
+
+  },
+});
 
 const AddServiceMesh = () => {
-    return (
-        <div>
-            Add Service Mesh
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <Card className={classes.card} raised="true" variant="outlined">
+        <CardContent className={classes.cardContent}>
+          <div className={classes.contentTop}>Top</div>
+          <div className={classes.contentBottom}>Bottom</div>
+        </CardContent>
+      </Card>
+      <Card className={classes.card} variant="outlined">
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Card 2
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className={classes.card} variant="outlined">
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Card 3
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
-export default AddServiceMesh
+export default AddServiceMesh;
