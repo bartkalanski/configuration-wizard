94174a7ec71db177f32b0231dd52344f8078d2c6
Bart Kalanski <bkalanski@gmail.com>
2021-03-14 18:48:29 +0000

    Add Modal component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,9 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Modal,
-  Button,
-  Checkbox,
-  Zoom,
 } from "@material-ui/core/";
 import BuildIcon from "@material-ui/icons/Build";
+import PopUp from './Modal'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,52 +28,9 @@ const useStyles = makeStyles((theme) => ({
   buildIcon: {
     cursor: "pointer",
   },
-  modal: {
-    position: "absolute",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-    height: "20rem",
-    padding: "1rem",
-    width: "500px",
-    marginLeft:"-250px",
-    left: "50%",
-    bottom: "35%",
-    backgroundColor: "#017374",
-    border: "none",
-    boxShadow: theme.shadows[5],
-  },
-  buttonContainer: {
-    alignSelf: "flex-end",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  checkbox: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  label: {
-    fontSize: "0.75rem",
-    color: "lightgray",
-    marginRight: "7rem",
-  },
-  startButton: {
-    padding: "0.5rem 2.25rem",
-    background: "#455A64",
-    color: "white",
-    "&:hover": {
-      backgroundColor: "#607D8B",
-    },
-  },
-  text: {
-    color: "white",
-  },
 }));
 
 const Navbar = () => {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -86,6 +40,8 @@ const Navbar = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
@@ -97,57 +53,7 @@ const Navbar = () => {
             </Typography>
           </Link>
           <BuildIcon onClick={handleOpen} className={classes.buildIcon} />
-
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-          >
-            <Zoom in={open}>
-              <div className={classes.modal}>
-                <Typography
-                  variant="h4"
-                  gutterBottom="true"
-                  paragraph="true"
-                  className={classes.text}
-                  id="simple-modal-title"
-                >
-                  Configuration Wizard
-                </Typography>
-                <Typography
-                  variant="body2"
-                  gutterBottom="true"
-                  paragraph="true"
-                  className={classes.text}
-                  id="simple-modal-description"
-                >
-                  Welcome to configuration wizard, Meshery's unique feature. The
-                  configuration wizard represents an assembly of existing
-                  functionality and settings that you can configure.
-                </Typography>
-                <div className={classes.buttonContainer}>
-                  <div className={classes.checkbox}>
-                    <Checkbox color="default" />
-                    <Typography className={classes.label}>
-                      Don't show this again
-                    </Typography>
-                  </div>
-                  <Button onClick={handleClose} className={classes.text}>
-                    Skip
-                  </Button>
-                  <Link to="/wizard" className={classes.link}>
-                    <Button
-                      onClick={handleClose}
-                      className={classes.startButton}
-                    >
-                      Get Started
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-            </Zoom>
-          </Modal>
+          <PopUp open={open} handleClose={handleClose} />
         </Toolbar>
       </AppBar>
     </div>
