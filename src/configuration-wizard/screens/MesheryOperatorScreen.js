fb929d1bde3d29f06b007ed6bb8dc6a0b93cc10b
Bart Kalanski <bkalanski@gmail.com>
2021-04-20 18:01:20 +0100

    Update styling as per new design

--- src/configuration-wizard/screens/MesheryOperatorScreen.js
+++ src/configuration-wizard/screens/MesheryOperatorScreen.js
@@ -3,7 +3,8 @@ import {
   makeStyles,
   Container,
   Fade,
-  Typography
+  Typography,
+  Button
 } from "@material-ui/core/";
 
 import mesheryOperatorIcon from "../../icons/mesheryOperator.svg";
@@ -11,38 +12,77 @@ import ConfigCard from "../reusecore/ConfigCard";
 
 const useStyles = makeStyles({
   cardContainer: {
+    position: 'relative',
     display: "flex",
     flexDirection: "row",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
     padding: "2rem 6rem",
   },
   infoContainer: {
+    position: 'relative',
     display: 'flex',
-    flexDirection: 'column',
-    marginTop: "2rem",
-    marginLeft: "1rem",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '20rem',
+    padding: '2rem 2rem',
+    marginTop: '2rem',
+    boxShadow: '0px 1px 6px 1px rgba(0,0,0,0.75)',
   },
   infoContext: {
-    fontSize: ".9rem"
+    fontSize: ".85rem"
   },
   infoKind: {
     fontSize: ".75rem",
+    marginTop: '.5rem',
     color: "#CACACA",
   },
+  settingsButton: {
+    position: 'absolute',
+    top: '23rem',
+    left: '2rem',
+    color: "#647881",
+    border: "1px solid #647881",
+    borderRadius: '.8rem',
+    fontSize: '.8rem',
+    fontWeight: '400',
+  }
 });
 
 const MesheryOperator = () => {
   const classes = useStyles();
+  const [state, setState] = React.useState(false);
+
+
+  const handleSwitch = (checked) => {
+    setState(checked)
+  }
+
 
   return (
     <Fade timeout={{ enter: "500ms" }} in="true">
       <Container className={classes.cardContainer}>
         {" "}
-        <ConfigCard name="Meshery Operator" icon={mesheryOperatorIcon} topInputPlaceholder="URL" bottomInputPlaceholder="" />
-        <div className={classes.infoContainer}>
-          <Typography className={classes.infoContext}>Context Name</Typography>
-          <Typography className={classes.infoKind}>kind-kind</Typography>
-        </div>
+        <ConfigCard handleSwitch={handleSwitch} name="Meshery Operator" icon={mesheryOperatorIcon} topInputPlaceholder="URL" bottomInputPlaceholder="" />
+        {!state ? null : (
+          <>
+            <div className={classes.infoContainer}>
+              <div>
+                <Typography className={classes.infoContext}>Operator</Typography>
+                <Typography className={classes.infoKind}>Disabled</Typography>
+              </div>
+              <div>
+                <Typography className={classes.infoContext}>MeshSync</Typography>
+                <Typography className={classes.infoKind}>Disabled</Typography>
+              </div>
+              <div>
+                <Typography className={classes.infoContext}>NATS</Typography>
+                <Typography className={classes.infoKind}>Disabled</Typography>
+              </div>
+            </div>
+            <Button className={classes.settingsButton}>Advanced Settings</Button>
+          </>
+        )}
+
       </Container>
     </Fade>
   );
